3fb28dc5a3f67cb05f5afcff175b2b259c48a23f
Dote3K <90593074+Dote3K@users.noreply.github.com>
2024-11-03 21:09:57 +0700

    temporary admin page

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -9,7 +9,8 @@ if (!isset($_SESSION['ma_khach_hang']) || $_SESSION['vai_tro'] !== 'admin') {
 echo "Admin";
 ?>
 <br>
-<a href="../admin/doanhthu/doanhthu.php"><button>Quản lý doanh thu</button></a><br>
+<a href="doanhthu/doanhthu.php"><button>Quản lý doanh thu</button></a><br>
+<a href="qlkh/quanlikhachhang.php"><button>Quản lý khách hàng</button></a>
 <a href="ql_sach/nxb/show_nxb.php"><button>Nhà xuất bản</button></a><br>
 <a href="ql_sach/tacgia/show_tacgia.php"><button>Tác Giả</button></a><br>
 <a href="ql_sach/theloai/show_the_loai.php"><button>Thể loại</button></a><br>
