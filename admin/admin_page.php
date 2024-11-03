8da040f4ff5a2752ec929870f61153bfe0e44aee
QuanAnh209 <buiquanganh209@gmail.com>
2024-11-03 10:42:02 +0700

    Quang_Anh

--- admin/admin_page.php
+++ admin/admin_page.php
@@ -8,7 +8,8 @@ if (!isset($_SESSION['ma_khach_hang']) || $_SESSION['vai_tro'] !== 'admin') {
 
 echo "Admin";
 ?>
-<a href="../admin/doanhthu/doanhthu.php">Quản lý doanh thu</a>
+<a href="../admin/doanhthu/doanhthu.php">Quản lý doanh thu</a><br>
+<a href="../admin/ql_sach/trang_quan_ly.php"></a>
 
 
 <a href="../home.php">Trở về trang chủ</a>
