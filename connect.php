5000898d1375993fa98bd0c5ab4d5a7844bf4513
QuanAnh209 <buiquanganh209@gmail.com>
2024-11-27 11:06:18 +0700

    sửa một chút

--- connect.php
+++ connect.php
@@ -1,22 +1,4 @@
-
 <?php
-/*class Database {
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "bookstore";
-
-    public function connect() {
-        $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
-        if ($conn->connect_error) {
-            die("Kết nối thất bại: " . $conn->connect_error);
-        }
-
-        return $conn;
-    }
-}*/
-
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -27,3 +9,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
+?>
